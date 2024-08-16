986299cb901f0af1e1e4972a727bbf29585f5d9c
Ezgi Kececi Atalay <ezgikececii@gmail.com>
2024-08-16 12:50:43 +0300

    Add files via upload

---
+++ PatikaLists2/PatikaLists2/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        List<string> coffee = new List<string>(); // Creating named coffee and string type list object
+       
+
+        Console.Write("Kahve isimleri:\n"); // Title
+        Console.WriteLine();
+
+        int i = 0;
+        while (true) 
+        {
+
+            Console.Write($"Kahve ismi {i + 1}: ");
+            i++;
+            string userResponse = Console.ReadLine(); // Get user input
+
+            coffee.Add(userResponse); // Add written items to list
+
+            if (coffee.Count == 5) // Number of coffee names can be 5
+            {
+                break; // break the loop
+            }
+
+            
+
+        }
+
+
+        Console.WriteLine("-----------------\nKahve Listesi:");
+        foreach (string item in coffee) // Print the list
+        {
+            Console.WriteLine(item);
+        }
+
+
+    }
+}
